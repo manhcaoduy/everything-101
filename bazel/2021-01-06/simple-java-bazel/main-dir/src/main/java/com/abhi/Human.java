bee786b2376538d5a9267a0171a433a6d3c1044a
Manh Cao <manh.cd@teko.vn>
2022-01-06 21:12:32 +0700

    init bazel

---
+++ bazel/2021-01-06/simple-java-bazel/main-dir/src/main/java/com/abhi/Human.java
@@ -0,0 +1,14 @@
+package com.abhi;
+
+import com.abhi.Animal;
+
+class Human implements Animal {
+	
+	public int numberOfLegs() {
+		return 2;
+	}
+
+	String sayHelloTo(String name) {
+		return "Hello" + name;
+	}
+}
